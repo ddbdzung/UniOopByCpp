9d0fcc178c3b6619cb7ce65bfa918763def4ae18
DzungDang <ddbdzung.wa@gmail.com>
2021-10-13 08:42:23 +0700

    add env file

--- Preparation/BTH_3_2.cpp
+++ Preparation/BTH_3_2.cpp
@@ -2,9 +2,26 @@
 
 using namespace std;
 
+class NSX {
+private:
+    string mansx;
+    string tennsx;
+    string dcnsx;
+public:
+    void nhap();
+    void xuat();
+    virtual ~NSX() {}
+};
+
+void NSX::nhap() {
+    cout << "Ma nha san xuat: ";    fflush(stdin);  getline(cin, mansx);
+    cout << "Ten nha san xuat: ";   fflush(stdin);  getline(cin, tennsx);
+    cout << "Dia chi nha san xuat: ";   fflush(stdin);  getline(cin, dcnsx);
+};
+
 int main() {
 
-
+    cout << "Hi";
 
     return 0;
 }
